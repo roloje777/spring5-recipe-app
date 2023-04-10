32a03a50f9207f1258bdc7540c4a468feaa7b3c5
Joao Rolo <roloje777@gmail.com>
2023-04-10 08:49:23 +0100

    adding mock mvc test example

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -11,11 +11,16 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.mockito.Mockito.*; // needed for the verify method
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,6 +42,15 @@ class IndexControllerTest {
 //        categoryRepository = Mockito.mock();
 
         controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
