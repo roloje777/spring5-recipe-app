624e702f8b504eb6ada1e8eef925e0d53218ccd4
Joao Rolo <roloje777@gmail.com>
2023-07-10 12:36:52 +0100

    add delete ingredient

--- src/main/java/guru/springframework/service/IngredientService.java
+++ src/main/java/guru/springframework/service/IngredientService.java
@@ -1,9 +1,12 @@
 package guru.springframework.service;
 
 import guru.springframework.commands.IngredientCommand;
+import guru.springframework.domain.Recipe;
 
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long id );
 }
