fd6cb4a0c59266c99fe9d98b0801e630daf8f422
Joao Rolo <roloje777@gmail.com>
2023-07-14 09:55:29 +0100

    add image upload - front end

---
+++ src/main/java/guru/springframework/service/ImageService.java
@@ -0,0 +1,8 @@
+package guru.springframework.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
