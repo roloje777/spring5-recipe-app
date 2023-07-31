4c8a68183271c47cad1f4dbfcaadbdd8dc4602c0
Joao Rolo <roloje777@gmail.com>
2023-07-31 16:06:03 +0100

    Adding handle numberformatexception

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -66,6 +66,16 @@ public class RecipeControllerTest {
 
     }
 
+    @Test
+    public void testNumberFormatException() throws Exception {
+      //  when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/xyz/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+
+    }
+
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
